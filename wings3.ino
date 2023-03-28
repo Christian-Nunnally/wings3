008bd36a42311d98a254f1b5912ad2954556db2b
Christian Nunnally <christian.nunnally@ni.com>
2023-03-28 08:47:39 -0500

    mic working

--- wings3.ino
+++ wings3.ino
@@ -1,60 +1,45 @@
-/*
-*** FIRST TIME HERE? START W/THE NEOPXL8 (not HDR) strandtest INSTEAD! ***
-That code explains and helps troubshoot wiring and NeoPixel color format.
-
-Adafruit_NeoPXL8HDR is a subclass of Adafruit_NeoPXL8 that provides
-16-bits-per-channel color, temporal dithering, and gamma correction.
-This is a minimal demonstration of "HDR" vs regular NeoPXL8 to show the
-extra setup required. It runs an animation loop similar to the non-HDR
-strandtest example, with the color fills on startup removed...those are
-for troubleshooting wiring and color order...work through that in the
-easier non-HDR strandtest and then copy your findings here. You'll
-find MUCH more documentation in that example.
-
-Most important thing to note here is that different chips (RP2040, SAMD51)
-require very different startup code. Animation loop is identical to NeoPXL8,
-just the setup changes. SAMD21 is NOT supported; HDR is just too demanding.
-SAMD51 requires the Adafruit_ZeroTimer library.
-*/
-
 #include <Adafruit_NeoPXL8.h>
 
-int8_t pins[8] = {0,1,2,3,4,5,6,7};
-#define COLOR_ORDER NEO_GRB // NeoPixel color format (see Adafruit_NeoPixel)
+#include <PDM.h>
+#include "SingleEMAFilterLib.h"
+
+#define COLOR_ORDER NEO_GRB
 #define NUM_LEDS    136      // NeoPixels PER STRAND, total number is 8X this!
+#define BAUD_RATE   9600
+#define BATCH_SIZE 512
+int8_t pins[8] = {0,1,2,3,4,5,6,7};
 
 Adafruit_NeoPXL8HDR leds(NUM_LEDS, pins, COLOR_ORDER);
+
+static const char channels = 1;
+static const int frequency = 16000;
+short sampleBuffer[512];
+volatile int samplesRead;
+
+SingleEMAFilter<int> singleEMAFilter(.25);
 
 void loop1() {
   leds.refresh();
 }
 
 void setup() {
-
-  // In Adafruit_NeoPXL8, begin() normally doesn't take arguments. The HDR
-  // version has some extra flags to select frame blending, added "depth"
-  // for temporal dithering, and whether to double-buffer DMA pixel writes:
-  if (!leds.begin(true, 4, true)) {
-    // If begin() returns false, either an invalid pin list was provided,
-    // or requested too many pixels for available memory (HDR requires
-    // INORDINATE RAM). Blink onboard LED to indicate startup problem.
-    pinMode(LED_BUILTIN, OUTPUT);
-    for (;;) digitalWrite(LED_BUILTIN, (millis() / 500) & 1);
+  Serial.begin(BAUD_RATE);
+  while (!Serial);
+  Serial.println("Serial Initalized.");
+  initializeLEDs();
+  PDM.onReceive(onPDMdata);
+  if (!PDM.begin(channels, frequency)) {
+    Serial.println("Failed to start PDM!");
+    while (1);
   }
-
-  // NeoPXL8HDR can provide automatic gamma-correction, but it's disabled
-  // by default; pixel values correlate to linear duty cycle. This is so
-  // existing NeoPixel or NeoPXL8 code brought over to NeoPXL8HDR looks the
-  // same, no surprises. When reworking a project for HDR, it's best to
-  // enable, animation code then doesn't need to process this. Gamma curve
-  // (2.6 in this example) is set at the same time as global brightness.
-  // Notice the brightness uses a 16-bit value (0-65535), not 8-bit
-  // (0-255) as in NeoPixel and NeoPXL8:
-  leds.setBrightness(65535/8, 2.6); // 1/8 max duty cycle, 2.6 gamma factor
 }
 
 int color = 0;
 int count = 0;
+int audioSamplesRead = 0;
+float audioSampleTotal = 0.0;
+float rms = 1.0;
+float rmsMax = 1.0;
 // ANIMATION LOOP (similar to "regular" NeoPXL8) ---------------------------
 
 // The loop() function is identical to the non-HDR example. Although
@@ -71,10 +56,65 @@ void loop() {
   }
   leds.show();
   count++;
-  if (count > 1200) {
+  if (count > 80) {
+    //Serial.println("l");
     count = 0;
     color = random(8);
   }
+
+  if (samplesRead) {
+    for (int i = 0; i < samplesRead; i++) {
+      //Serial.println(sampleBuffer[i]);
+      
+      // singleEMAFilter.AddValue(sampleBuffer[i]);
+      // Serial.print(sampleBuffer[i]);
+      // Serial.print(",\t");
+      // Serial.print(singleEMAFilter.GetLowPass());
+      // Serial.print(",\t");
+      // Serial.println(singleEMAFilter.GetHighPass());
+
+      audioSampleTotal += sampleBuffer[i] * sampleBuffer[i];
+      audioSamplesRead += 1;
+      if (audioSamplesRead == BATCH_SIZE)
+      {
+        rms = sqrt(audioSampleTotal / audioSamplesRead);
+        audioSamplesRead = 0;
+        audioSampleTotal = 0;
+        if (rms > rmsMax)
+        {
+          rmsMax = rms;
+        }
+        if (rmsMax > 1) rmsMax = rmsMax - 1;
+        singleEMAFilter.AddValue(rms);
+        Serial.print(rms);
+        Serial.print(",");
+        Serial.print(singleEMAFilter.GetLowPass());
+        Serial.print(",");
+        Serial.print(singleEMAFilter.GetHighPass());
+        Serial.print(",");
+        Serial.print(rmsMax);
+        Serial.print(",");
+        Serial.println(1200);
+      }
+    
+
+    }
+    samplesRead = 0;
+  }
+}
+
+void initializeLEDs()
+{
+  bool blendFrames = true;
+  uint8_t temporalDitheringAmount = 4; // 0-8
+  bool doubleBuffer = true;
+  if (!leds.begin(blendFrames, temporalDitheringAmount, doubleBuffer, pio1) && Serial) 
+  {
+    Serial.println("Error: Not enough RAM for LEDs.");
+  }
+  float gamma = 2.6;
+  uint16_t dutyCycle = 65535;
+  leds.setBrightness(dutyCycle, gamma);
 }
 
 
@@ -90,14 +130,26 @@ static uint8_t colors[8][3] = {
   255,   0, 255  // Row 7: Magenta
 };
 
-// This function changes very slightly from the non-HDR strandtest example.
-// That code had to apply gamma correction on is own. It's not necessary
-// with the HDR class once configured (see setBrightness() in setup()),
-// we get that "free" now.
 uint32_t rain(uint32_t now, uint8_t row, int pixelNum) {
   uint8_t frame = now / 4; // uint8_t rolls over for a 0-255 range
-  uint16_t b = 256 - ((frame - row * 32 + pixelNum * 256 / NUM_LEDS) & 0xFF);
+  uint16_t b = (256 - ((frame - row * 32 + pixelNum * 256 / NUM_LEDS) & 0xFF)) * (rms / rmsMax);
   return leds.Color((colors[(color + pixelNum) % 8][0] * b) >> 8,
                     (colors[(color + pixelNum) % 8][1] * b) >> 8,
                     (colors[(color + pixelNum) % 8][2] * b) >> 8);
+}
+
+/**
+   Callback function to process the data from the PDM microphone.
+   NOTE: This callback is executed as part of an ISR.
+   Therefore using `Serial` to print messages inside this function isn't supported.
+ * */
+void onPDMdata() {
+  // Query the number of available bytes
+  int bytesAvailable = PDM.available();
+
+  // Read into the sample buffer
+  PDM.read(sampleBuffer, bytesAvailable);
+
+  // 16-bit, 2 bytes per sample
+  samplesRead = bytesAvailable / 2;
 }
