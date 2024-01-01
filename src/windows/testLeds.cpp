843c718a6e7c4e3f1fe11cb6309b24c6eb5a7487
Christian Nunnally <christian.nunnally@ni.com>
2024-01-01 00:29:07 -0600

    .

--- src/windows/testLeds.cpp
+++ src/windows/testLeds.cpp
@@ -1,17 +1,21 @@
 #include "../windows/testLeds.h"
+#include "../common/settings.h"
 
 uint8_t testLedGlobalBrightnessValue = 255;
+
+
 
 void setTestLedsPixelColor(int pixelIndex, uint8_t red, uint8_t green, uint8_t blue)
 {
     std::cout << STANDARD_OUTPUT_OPERATION_CODE_SET_LED;
     std::cout << ",";
-    std::cout << pixelIndex;
+    std::cout << std::hex << pixelIndex << std::dec;
     std::cout << ",";
     uint8_t brightnessAdjustedRed = (red * testLedGlobalBrightnessValue) >> 8;
     uint8_t brightnessAdjustedGreen = (green * testLedGlobalBrightnessValue) >> 8;
     uint8_t brightnessAdjustedBlue = (blue * testLedGlobalBrightnessValue) >> 8;
-    std::cout << (uint32_t)((brightnessAdjustedRed << 16) | (brightnessAdjustedGreen << 8) | (brightnessAdjustedBlue << 0));
+    uint32_t colorAsNumber = (uint32_t)((brightnessAdjustedRed << 16) | (brightnessAdjustedGreen << 8) | (brightnessAdjustedBlue << 0));
+    std::cout << std::hex << colorAsNumber << std::dec;
     std::cout << "\n";
 }
 
